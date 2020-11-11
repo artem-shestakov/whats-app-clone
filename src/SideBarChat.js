8afc93f471c24b219d83d4191e424f5a0550dfb4
Artem Shestakov <artem.s.shestakov@yandex.ru>
2020-11-11 23:50:40 +0300

    Add link to rooms, change avatar in chat like room avatar

--- src/SideBarChat.js
+++ src/SideBarChat.js
@@ -2,6 +2,7 @@ import React, {useEffect, useState} from 'react'
 import './SideBarChat.css'
 import {Avatar} from "@material-ui/core";
 import db from "./firebase"
+import {Link} from "react-router-dom";
 
 function SideBarChat({id, name, addNewChat }) {
     const [seed, setSeed] = useState('');
@@ -23,13 +24,15 @@ function SideBarChat({id, name, addNewChat }) {
     };
 
     return !addNewChat ? (
-        <div className='SideBarChat'>
-            <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`} />
-            <div className="SideBarChatInfo">
-                <h2>{name}</h2>
-                <p>Last message...</p>
+        <Link to={`/room/${id}`}>
+            <div className='SideBarChat'>
+                <Avatar src={`https://avatars.dicebear.com/api/human/${id}.svg`} />
+                <div className="SideBarChatInfo">
+                    <h2>{name}</h2>
+                    <p>Last message...</p>
+                </div>
             </div>
-        </div>
+        </Link>
     ) : (
         <div className="SideBarChat" onClick={createChat}>
             <h2>Add new Chat</h2>
